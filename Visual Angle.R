36d5f466bbe570a29943224664688a5b113eaffe
JannaMra <janna.teigeler@uni-wuerzburg.de>
2023-08-30 15:48:36 +0200

    Skript Visual Angle

---
+++ Visual Angle.R
@@ -0,0 +1,21 @@
+###########################################################
+# Calculation of visual angles
+
+#rm(list=ls())
+
+# Screen dimensions: LG 24MB65PY-B
+# Pixel size
+wx <- 1920; wy <- 1200
+# Screen size (in cm)
+swx <- 51.694; swy <- 32.309
+# Screen distance sdist (in cm)
+sdist <- 59
+
+# Calculate visual angles for whole screen
+vax <- 2*(atan((swx/2)/sdist)/pi*180)
+vay <- 2*(atan((swy/2)/sdist)/pi*180)
+
+# Pixels per 1 deg of visual angle
+xdeg <- wx/vax
+ydeg <- wy/vay
+
