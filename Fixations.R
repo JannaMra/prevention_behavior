d01c39f8839c2e6882062defe1de5f5bd416630c
JannaMra <janna.teigeler@uni-wuerzburg.de>
2023-09-22 17:27:50 +0200

    Analyses of fixations on picture during onset

--- Fixations.R
+++ Fixations.R
@@ -64,15 +64,15 @@ for (vpn in vps) {
 protfixa <- protfixa %>%
   arrange(vp, trial)
 
-# use only first saccade and only trials with valid baseline
-firstprotfixa <- protfixa %>%
+# use only last fixation and only trials with valid baseline
+lastprotfixa <- protfixa %>%
   group_by(vp, trial)%>%
   filter(blok==1)%>%
-  filter(row_number()==1)%>%
+  filter(row_number()== n())%>%
   ungroup()
 
 # correct for individual baselines
-firstprotfixa <- firstprotfixa %>%
+lastprotfixa <- lastprotfixa %>%
   mutate(x = x-blmeanx,
          y = (y-blmeany)*(-1)) %>%
   filter(case_when(operant==0 ~ response == 0,  #filter out false or multiple responses
@@ -81,7 +81,7 @@ firstprotfixa <- firstprotfixa %>%
   mutate(OpPrev = ifelse(valence == 1,"operant", "prevent"),
          ActPass = ifelse(operant == 1, "active", "passive")) 
 
-plotfirstfixa <- firstprotfixa %>%
+plotlastfixa <- lastprotfixa %>%
   group_by(OpPrev, ActPass)%>%
   summarise(
     mean = mean(y),
@@ -91,9 +91,9 @@ plotfirstfixa <- firstprotfixa %>%
   )
 
 
-ez::ezANOVA(data = firstprotfixa, dv = y, wid = vp, within = c(OpPrev, ActPass), detailed = TRUE) %>% schoRsch::anova_out()
+ez::ezANOVA(data = lastprotfixa, dv = y, wid = vp, within = c(OpPrev, ActPass), detailed = TRUE) %>% schoRsch::anova_out()
 
-ggplot(data = plotfirstfixa, mapping = aes(x = OpPrev, y = mean, group = ActPass, color = ActPass)) +
+ggplot(data = plotlastfixa, mapping = aes(x = OpPrev, y = mean, group = ActPass, color = ActPass)) +
   geom_point(size = 3) +
   geom_path(linewidth = 1) +
   geom_errorbar(aes(ymin = mean - se, ymax = mean + se), width = 0.05) +
@@ -101,22 +101,22 @@ ggplot(data = plotfirstfixa, mapping = aes(x = OpPrev, y = mean, group = ActPass
   ylab("Distance of last Fixation during Anticipation from Cue-Position +- SE") +
   theme_classic()
 
-firstprotfixa <- firstprotfixa %>% group_by(vp, OpPrev, ActPass) %>%
+lastprotfixa_t.test <- lastprotfixa %>% group_by(vp, OpPrev, ActPass) %>%
   summarise(y = mean(y))
 
-t.test(y ~ OpPrev, data= (firstprotfixa %>% filter(ActPass == "passive")), paired = T) %>% schoRsch::t_out()
-t.test(y ~ OpPrev, data= (firstprotfixa %>% filter(ActPass == "active")), paired = T) %>% schoRsch::t_out()
+t.test(y ~ OpPrev, data= (lastprotfixa_t.test %>% filter(ActPass == "passive")), paired = T) %>% schoRsch::t_out()
+t.test(y ~ OpPrev, data= (lastprotfixa_t.test %>% filter(ActPass == "active")), paired = T) %>% schoRsch::t_out()
 
-t.test(y ~ ActPass, data= (firstprotfixa %>% filter(OpPrev == "prevent")), paired = T) %>% schoRsch::t_out()
-t.test(y ~ ActPass, data= (firstprotfixa %>% filter(OpPrev == "operant")), paired = T) %>% schoRsch::t_out()
+t.test(y ~ ActPass, data= (lastprotfixa_t.test %>% filter(OpPrev == "prevent")), paired = T) %>% schoRsch::t_out()
+t.test(y ~ ActPass, data= (lastprotfixa_t.test %>% filter(OpPrev == "operant")), paired = T) %>% schoRsch::t_out()
 
-t.test(y ~ OpPrev, data= (firstprotfixa %>% filter((OpPrev == "prevent" & ActPass == "active")|(OpPrev == "operant" & ActPass == "passive"))), paired = T) %>% schoRsch::t_out()
-t.test(y ~ OpPrev, data= (firstprotfixa %>% filter((OpPrev == "operant" & ActPass == "active")|(OpPrev == "prevent" & ActPass == "passive"))), paired = T) %>% schoRsch::t_out()
+t.test(y ~ OpPrev, data= (lastprotfixa_t.test %>% filter((OpPrev == "prevent" & ActPass == "active")|(OpPrev == "operant" & ActPass == "passive"))), paired = T) %>% schoRsch::t_out()
+t.test(y ~ OpPrev, data= (lastprotfixa_t.test %>% filter((OpPrev == "operant" & ActPass == "active")|(OpPrev == "prevent" & ActPass == "passive"))), paired = T) %>% schoRsch::t_out()
 
 
 # Percent of Fixations that lies in upper half of screen 
 
-fixaupperhalf <- firstprotfixa %>%
+fixaupperhalf <- lastprotfixa %>%
   mutate(upperhalf = ifelse(y > 150, 1,0)) 
 
 fixaupperhalf %>%
@@ -124,8 +124,121 @@ fixaupperhalf %>%
   summarise(
     percent = sum(upperhalf)/length(upperhalf)
   )
+
+# Percent of fixations that lies within picture (anticipation)
+
+fixapicture <- lastprotfixa %>% ungroup()%>%
+  mutate(picture_y = ifelse((y >= 212.5 & y <= 587.5),1,0),       #y-coordinates of picture (if cue is origin) picture: 500 x 375 pixels, center 400 pixels away from cue center
+         picture_x = ifelse((x >= -250 & x <= 250),1,0),          #x-coordinates of picture (if cue is origin)
+         picture = ifelse((picture_x == 1 & picture_y == 1),1,0)) 
+
+fixapicture %>%
+  group_by(ActPass, OpPrev) %>%
+  summarise(
+    percent = sum(picture)/length(picture)
+  )
+
+
+
+## Percent of fixation that lies within picture (during onset) - operant active and prevention passive only --------------------------------------------------------------------------------
+
+picturefixa <- data.frame()
+protpicturefixa <- data.frame()
+
+for (vpn in vps) {
+  #vpn = "vp05"
+  vpcode <- vpn
+  code <- vpn
+  print(code)
+  ntrial <- 200
   
+  
+  # Loop over trials to determine trial-by-trial baselines
+  for (trial in 1:ntrial) {
+    #trial = 1
+    
+    # Select trial data
+    fixablock <- fixa[
+      tolower(fixa$vp)==code & #tolower = translates characters in character vectors
+        fixa$trial==trial,]
+    onsetblock <- onset[
+      tolower(onset$vp)==code & #tolower = translates characters in character vectors
+        onset$trial==trial,]
+    
+    # Filter
+    fixablock <- fixablock %>%
+      filter(timeend >= onsetblock$time & timest <= onsetblock$time+2700)
+    
+    # write dataframe with anticipatory saccades
+    picturefixa <- bind_rows(picturefixa,fixablock)
+  }
+  
+  # Combine with trial-info
+  #seqdat <- read.csv2(paste("Data/Analyse/prot/",vpn,"_BL.csv",sep=""))
+  #seqdatall <- bind_rows(seqdatall,seqdat)
+  protpicturefixa <- merge(seqdatall, picturefixa, by = c("vp", "trial"))
+}
 
+protpicturefixa <- protpicturefixa %>%
+  arrange(vp, trial)
 
+# use only trials with valid baseline
+protpicturefixa <- protpicturefixa %>%
+  group_by(vp, trial)%>%
+  filter(blok==1)%>%
+  ungroup()
+
+# use only trials where picture is shown
+protpicturefixa <- protpicturefixa %>%
+  filter((valence==1 & operant ==1)|(valence == 0 & operant == 0))  #positv aktiv, aversiv passiv = hier kommen Bilder
+
+# correct for individual baselines
+protpicturefixa <- protpicturefixa %>%
+  mutate(x = x-blmeanx,
+         y = (y-blmeany)*(-1)) %>%
+  filter(case_when(operant==0 ~ response == 0,  #filter out false or multiple responses
+                   operant==1 ~ response == 1)
+  )%>%
+  mutate(OpPrev = ifelse(valence == 1,"operant", "prevent"),
+         ActPass = ifelse(operant == 1, "active", "passive")) 
+
+# fixation lies within picture region? 
+protpicturefixa <- protpicturefixa %>% ungroup()%>%
+  mutate(picture_y = ifelse((y >= 212.5 & y <= 587.5),1,0),       #y-coordinates of picture (if cue is origin) picture: 500 x 375 pixels, center 400 pixels away from cue center
+         picture_x = ifelse((x >= -250 & x <= 250),1,0),          #x-coordinates of picture (if cue is origin)
+         picture = ifelse((picture_x == 1 & picture_y == 1),1,0)) %>%
+  mutate(duration = timeend-timest) %>%
+  mutate(weighted_fixation = picture * duration) %>%
+  group_by(vp, trial)%>%
+  mutate(sum_duration = sum(duration),
+         sum_fixation = sum(weighted_fixation),
+         percent = sum_fixation/sum_duration)
+
+plotpicturefixa <- protpicturefixa %>%
+  group_by(ActPass, OpPrev) %>%
+  summarise(
+    mean_percent = mean(percent),
+    sd = sd(percent),
+    se = se(percent)
+  )
+
+plotpicturefixa_test <- protpicturefixa %>%
+  group_by(vp, OpPrev) %>%
+  summarise(
+    mean_percent = mean(percent),
+    sd = sd(percent),
+    se = se(percent)
+  )
+
+ggplot(data = plotpicturefixa, mapping = aes(x = OpPrev, y = mean_percent, fill = OpPrev)) +
+  geom_point(size = 3) +
+  geom_path(linewidth = 1) +
+  geom_errorbar(aes(ymin = mean_percent - se, ymax = mean_percent + se), width = 0.05) +
+  geom_violin(data = plotpicturefixa_test, mapping=aes(x= OpPrev, y = mean_percent), alpha = .2) +
+  scale_color_manual(labels = c("active", "passive"), values = c("#6BBFA3", "#007AC3")) +
+  ylab("Percent of Time spent looking at picture +- SE") +
+  theme_classic()
+
+t.test(mean_percent ~ OpPrev, data = plotpicturefixa_test, paired = T) %>% schoRsch::t_out()
 
 
