e0dde5ca9ed81b2db06e87bcef22a8d2ad26cab8
JannaMra <janna.teigeler@uni-wuerzburg.de>
2024-11-07 15:19:06 +0100

    Renaming conditions

--- 16_Pupil_Analysis.R
+++ 16_Pupil_Analysis.R
@@ -2,6 +2,7 @@
 # Prevention Behavior project
 # Solveig Tonn & Janna Teigeler
 #
+library(tidyverse) 
 
 # Onsets laden
 msg <- read.csv2(paste0(path,"Data/Eyelink/msg.csv"), row.names = 1)
@@ -117,7 +118,7 @@ print(pupil.plot %>% ggplot(aes(x=time, y=mmChange, color=condition, group=condi
 
 
 # Analyze just Anticipation Period ----------------------------------------------------------------------------------------------------
-
+#pupil_long <- read.csv2(paste(path,"Data/Pupil/","Pupil_long.csv",sep=""))%>% select(-1)
 pupildat <- data.frame()
 
 for (vpn in vps) {
@@ -177,14 +178,14 @@ pupil.plot = pupil_long %>% group_by(condition, time) %>%
   summarise(mmChange.se = se(mmChange, na.rm=T), mmChange = mean(mmChange, na.rm=T))
 
 
-print(pupil.plot %>% ggplot(aes(x=time, y=mmChange, group=condition)) + 
+pupil.plot.1 <- pupil.plot %>% ggplot(aes(x=time, y=mmChange, group=condition)) + 
         #geom_dotplot(data=pupil.ga.gen.subj, mapping=aes(group=threat, fill=threat), binaxis="y", alpha=.25, color="black", stackratio=1, stackdir="centerwhole", dotsize=.5) +
         #geom_point() + geom_path(data=pupil.ga.gen %>% dplyr::filter(threat %in% c("CS-", "CS+")), color = "black", size=1.5) + #generalization line (geom_point first for order of x-axis)
         geom_line(aes(colour = condition, linetype = condition), linewidth = 1) +
         geom_ribbon(aes(ymin=mmChange-mmChange.se, ymax=mmChange+mmChange.se, fill= condition), color =NA, alpha=.1) + 
-        scale_linetype_manual(name="Trial type", values=c("solid", "dotdash", "solid", "dotdash"), labels = c("Operant active","Operant passiv","Prevent active", "Prevent passive"))+
-        scale_color_manual(name="Trial type", values = c("#708B50","#708B50","#E88067", "#E88067"),labels = c("Operant active","Operant passiv","Prevent active", "Prevent passive")) +
-        scale_fill_manual(name="Trial type", values = c("#708B50","#708B50","#E88067", "#E88067"),labels = c("Operant active","Operant passiv","Prevent active", "Prevent passive")) +
+        scale_linetype_manual(name="Trial type", values=c("solid", "dotdash", "solid", "dotdash"), labels = c("active production","passiv production","active prevention", "passive prevention"))+
+        scale_color_manual(name="Trial type", values = c("#708B50","#708B50","#E88067", "#E88067"),labels = c("active production","passiv production","active prevention", "passive prevention")) +
+        scale_fill_manual(name="Trial type", values = c("#708B50","#708B50","#E88067", "#E88067"),labels = c("active production","passiv production","active prevention", "passive prevention")) +
         #scale_color_manual(values = c("#6BBFA3", "#007AC3")) +
         #scale_colour_viridis_d() +
         geom_line(aes(x = 0), color = "black") +
@@ -196,7 +197,41 @@ print(pupil.plot %>% ggplot(aes(x=time, y=mmChange, group=condition)) +
                  angle = 90,
                  vjust = 1) +
         ylab("Pupil Size Change (mm)") + xlab("Time") + labs(color="condition") +
-        theme_classic())
+        ggtitle("Experiment 1")+
+        theme_classic()+
+        theme(axis.text=element_text(size=14),
+              axis.title=element_text(size=16),
+              plot.title=element_text(size=18),
+              legend.position = c(0.2, 0.8))
+
+pupil.plot.2 <- pupil.plot %>% ggplot(aes(x=time, y=mmChange, group=condition)) + 
+  #geom_dotplot(data=pupil.ga.gen.subj, mapping=aes(group=threat, fill=threat), binaxis="y", alpha=.25, color="black", stackratio=1, stackdir="centerwhole", dotsize=.5) +
+  #geom_point() + geom_path(data=pupil.ga.gen %>% dplyr::filter(threat %in% c("CS-", "CS+")), color = "black", size=1.5) + #generalization line (geom_point first for order of x-axis)
+  geom_line(aes(colour = condition, linetype = condition), linewidth = 1) +
+  geom_ribbon(aes(ymin=mmChange-mmChange.se, ymax=mmChange+mmChange.se, fill= condition), color =NA, alpha=.1) + 
+  scale_linetype_manual(name="Trial type", values=c("solid", "dotdash", "solid", "dotdash"), labels = c("Operant active","Operant passiv","Prevent active", "Prevent passive"))+
+  scale_color_manual(name="Trial type", values = c("#708B50","#708B50","#E88067", "#E88067"),labels = c("Operant active","Operant passiv","Prevent active", "Prevent passive")) +
+  scale_fill_manual(name="Trial type", values = c("#708B50","#708B50","#E88067", "#E88067"),labels = c("Operant active","Operant passiv","Prevent active", "Prevent passive")) +
+  #scale_color_manual(values = c("#6BBFA3", "#007AC3")) +
+  #scale_colour_viridis_d() +
+  geom_line(aes(x = 0), color = "black") +
+  #geom_line(aes(x = 220), linetype = "dashed", color = "black") +
+  annotate(geom = "text",
+           label = c("Picture Onset"),
+           x = c(10),
+           y = c(0),
+           angle = 90,
+           vjust = 1) +
+  ylab("Pupil Size Change (mm)") + xlab("Time") + labs(color="condition") +
+  #ggtitle("Experiment 1")+
+  ggtitle("Experiment 2")+
+  theme_classic()+
+  theme(axis.text=element_text(size=14),
+        axis.title=element_text(size=16),
+        plot.title=element_text(size=18),
+        legend.position = "none")
+
+cowplot::plot_grid(pupil.plot.1, pupil.plot.2, labels = c('A)', 'B)'), label_size = 14)
 
 write.csv2(pupil_long,paste(path,"Data/Pupil/","Pupil_long.csv",sep=""))
 
